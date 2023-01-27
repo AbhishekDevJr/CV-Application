9aabe402d901b57fcd3e0c77b773426cf51d4931
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-01-27 18:59:34 +0530

    PersonalInfo Component inprogress

--- cv-app/src/components/PersonalInfo.js
+++ cv-app/src/components/PersonalInfo.js
@@ -6,62 +6,116 @@ class PersonalInfo extends Component{
         super(props);
 
         this.state = {
-            fname : ''
+            fname : '',
+            lname : '',
+            title : '',
+            address : '',
+            number : '',
+            email : '',
+            description : ''
         };
+
     }
 
     handleFirstName = (e) => {
-        console.log(e.target.value);
+
         this.setState({
-            fname : e.target.value,
+            fname : e.target.value
+        },() => {
+            console.log("New state in ASYNC callback:", this.state.fname + ' ' + this.state.lname );
+          });
+        console.log('State Object--> ' , this.state);
+    }
+
+    handleLastName = (e) => {
+        this.setState({
+            lname : e.target.value
+        }, () => {
+            console.log("New state in ASYNC callback:", this.state.fname + ' ' + this.state.lname );
+          });
+    }
+
+    handleTitle = (e) => {
+        this.setState({
+            title : e.target.value
         });
-        console.log(this.state.fname);
+    }
+
+    handlePhoto = (e) => {
+        console.log('Photo-->', e, ' Will implement handling pictures feature later');
+    }
+
+    handleAddress = (e) => {
+        this.setState({
+            address : e.target.value
+        });
+    }
+
+    handleNumber = (e) => {
+        this.setState({
+            number : e.target.value
+        });
+    }
+
+    handleEmail = (e) => {
+        this.setState({
+            email : e.target.valule
+        });
+    }
+
+    handleDescription = (e) => {
+        this.setState({
+            description : e.target.value
+        });
     }
 
     render(){
         return(
             <div className = "personal-info">
-                <header><h1>CV Generator</h1></header>
+                <header>
+                    <h1>CV Generator</h1>
+                    <p>Personal Information</p>
+                </header>
                 <div className = "section-personalInfo">
                     <form action = "" method = "">
                         <div className = "row">
                         <label htmlFor = "name">First Name</label>
-                        <input type = "text" id = "name" name = "name" placeholder = "First Name" onChange={this.handleFirstName}/>
+                        <input type = "text" id = "name" name = "name" value={this.state.fname} placeholder = "First Name" onChange={this.handleFirstName}/>
                         </div>
 
                         <div className = "row">
                         <label htmlFor = "lName">Last Name</label>
-                        <input type = "text" id = "lName" name = "lName" />
+                        <input type = "text" id = "lName" name = "lName" onChange = {this.handleLastName} />
                         </div>
 
                         <div className = "row">
                         <label htmlFor = "title">Title</label>
-                        <input type = "text" id = "title" name = "title" />
+                        <input type = "text" id = "title" name = "title" onChange = {this.handleTitle} />
                         </div>
 
                         <div className = "row">
                         <label htmlFor = "photo">Photo</label>
-                        <input type = "file" id = "photo" name = "photo" />
+                        <input type = "file" id = "photo" name = "photo" onChange = {this.handlePhoto} />
                         </div>
 
                         <div className = "row">
                         <label htmlFor = "address">Address</label>
-                        <input type = "text" id = "address" name = "Address" />
+                        <input type = "text" id = "address" name = "Address" onChange = {this.handleAddress}/>
                         </div>
 
                         <div className = "row">
                         <label htmlFor = "number">Phone Number</label>
-                        <input type = "text" id = "number" name = "Number" />
+                        <input type = "text" id = "number" name = "Number" onChange = {this.handleNumber} />
                         </div>
 
                         <div className = "row">
                         <label htmlFor = "email">Email</label>
-                        <input type = "email" id = "email" name = "Email" />
+                        <input type = "email" id = "email" name = "Email" onChange = {this.handleEmail} />
                         </div>
 
                         <div className = "row">
                         <label htmlFor = "description">Description</label>
-                        <textarea id = "description" name = "Description"></textarea>
+                        <textarea id = "description" name = "Description" onChange = {this.handleDescription} ></textarea>
                         </div>
 
                     </form>
