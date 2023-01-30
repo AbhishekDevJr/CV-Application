61f83b3b4f4c011c2b29257d4c3aac65100ca1fe
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-01-30 19:48:24 +0530

    DisplayComp in progress

--- cv-app/src/components/DisplayComp.js
+++ cv-app/src/components/DisplayComp.js
@@ -4,12 +4,31 @@ import {Component} from 'react';
 class DisplayComp extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            isClicked : false
+        }
+    }
+
+    handleBtnClick = (e) => {
+        this.setState({
+            isClicked : true
+        });
     }
 
     render(){
+
+        if(this.state.isClicked){
+            let cvContent = <div className = "display-cv">Conditional Rendering Working</div>
+        }else{
+            let cvContent = <div className = ""></div>
+        }
+
         return(
             <div className = "display-comp">
-                Display Component Working
+                {/* {this.state.isClicked ? (console.log('Yes Clicked') : (console.log('Not Clicked')))} */}
+                {this.cvContent}
+                <button className = "display-handler" onClick = {this.handleBtnClick}>Create CV</button>
             </div>
         );
     }
